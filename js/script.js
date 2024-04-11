af53c91d44bf38ca3da1e69fec84f9544cb2e113
felipermendess <felipermendes04@gmail.com>
2024-04-11 10:28:55 -0400

    parametros-url-adicionados

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativando links
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +10,15 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+//Ativando url
+const url = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+url.forEach(ativarProduto);
