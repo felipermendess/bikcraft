98040c85a077461c5abd31b4dbb0c40bfb512d9a
felipermendess <felipermendes04@gmail.com>
2024-04-15 11:28:42 -0400

    ativando-galeria

--- js/script.js
+++ js/script.js
@@ -41,3 +41,20 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Ativando Galeria
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagens(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+function eventosGaleria(imagem) {
+  imagem.addEventListener("click", trocarImagens);
+}
+galeria.forEach(eventosGaleria);
